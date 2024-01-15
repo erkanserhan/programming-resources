7b277bbc1994208390be1a836ea03b0a91db5b9e
iframe <86450406+vmhl87@users.noreply.github.com>
2024-01-14 23:20:58 -0500

    add exercise solutions

---
+++ lesson-3/factorial.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int input;
+	cin >> input;
+
+	int product = 1; // We can't set it to 0
+	for(int iteration = 1; iteration < input; iteration++) {
+		product *= iteration;
+	}
+
+	cout << product << endl;
+}
